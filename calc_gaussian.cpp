f42ab8f1b5f5ba228d8d3f4754fcb7124b99a71e
Jared Leach <leach.jared@gmail.com>
2019-04-24 17:21:58 -0700

    started implementation of update cycle for measurement and state

--- calc_gaussian.cpp
+++ calc_gaussian.cpp
@@ -20,9 +20,30 @@ tuple<double, double> measurement_update(double prior_mean, double prior_varianc
 	double new_var = 1 / ((1 / measurement_variance) + (1 / prior_variance));
 	return  make_tuple(new_mean, new_var);
 }
+tuple<double, double> state_prediction(double prior_mean, double prior_variance, double motion_mean, double motion_variance){
+	// TODO: Code the measurement update mean function mu;
+	double new_mean = prior_mean + motion_mean;
+	// TODO: Code the measurement update variance function sigma squared
+	double new_var = prior_variance + motion_variance;
+	return  make_tuple(new_mean, new_var);
+}
 
 
 int main(){
-	cout << f(10.0, 4.0, 8.0) << endl;
+	double measurements[5] = {5, 6, 7, 9, 10};
+	double measurement_sig = 4;
+	
+	double motion[5] = {1, 1, 2, 1, 1};
+	double motion_sig = 2;
+	
+	double mu = 0;
+	double sig = 1000;
+	// TODO: Put code below this
+	// TODO: Loop through all the measurements
+		// TODO: Apply a measurement update
+		cout << "update:[" << mu << "][" << sig << "]" << endl;
+		// TODO: Apply a state prediction
+		cout << "update:[" << mu << "][" << sig << "]" << endl;
+
 	return 0;
 }
